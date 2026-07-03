d4205948597bdec159a668ce271d7636240b9630
Divakar176 <your-email@gmail.com>
2026-07-03 18:02:57 +0530

    Fix URI parsing for database connection

--- backend/src/main/java/com/pavithra/erp/config/DataSourceConfig.java
+++ backend/src/main/java/com/pavithra/erp/config/DataSourceConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class DataSourceConfig {
@@ -20,13 +22,25 @@ public class DataSourceConfig {
     private String databasePassword;
 
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource() throws URISyntaxException {
         if (databaseUrl != null && databaseUrl.startsWith("postgres://")) {
-            String dbUrl = databaseUrl.replace("postgres://", "jdbc:postgresql://");
+            URI dbUri = new URI(databaseUrl);
+            String username = databaseUsername;
+            String password = databasePassword;
+            if (dbUri.getUserInfo() != null) {
+                String[] userInfo = dbUri.getUserInfo().split(":");
+                username = userInfo[0];
+                if (userInfo.length > 1) {
+                    password = userInfo[1];
+                }
+            }
+            int port = dbUri.getPort() == -1 ? 5432 : dbUri.getPort();
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath();
+
             return DataSourceBuilder.create()
                     .url(dbUrl)
-                    .username(databaseUsername)
-                    .password(databasePassword)
+                    .username(username)
+                    .password(password)
                     .driverClassName("org.postgresql.Driver")
                     .build();
         }
@@ -34,7 +48,7 @@ public class DataSourceConfig {
                 .url(databaseUrl)
                 .username(databaseUsername)
                 .password(databasePassword)
-                .driverClassName("org.postgresql.Driver") // Force postgresql driver since we are switching to Postgres on production
+                .driverClassName("org.postgresql.Driver")
                 .build();
     }
 }
