6f0df11e305b91beef2a248cd95dc9227b4c088d
divakar176 <diva.es176@gmail.com>
2026-07-09 11:05:46 +0530

    Use vite plugin legacy for maximum browser compatibility

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    target: 'es2015'
-  }
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ]
 })
